bd965a5546b15184a8331e74bbbbb0b19520eda9
golony6449 <bb0162@live.co.kr>
2017-12-04 13:42:43 +0900

    error, welcome 페이지의 이미지 수정 View 예외처리 (error 페이지로 리다이렉트)

--- web/blog/views.py
+++ web/blog/views.py
@@ -30,7 +30,8 @@ def login(request):
     else:
         # print('fail')
         # return HttpResponse('로그인 실패 다시시도 해보세요.')
-        return HttpResponseRedirect(reverse('index'))
+        paraDic={'errCode': 4}
+        return render(request, 'test/error.html', paraDic)
 
     # return render(request,'blog/login.html')
 
@@ -40,15 +41,16 @@ def logout(request):
 
 def write(request):
     if not request.user.is_authenticated():
-        return HttpResponseRedirect('/')
+        paraDic={'errCode': 2}
+        return render(request,'test/error.html', paraDic)
 
     paraDic={'post':None, 'edit':False}
-
     return render(request,'test/write.html', paraDic)
 
 def regpost(request, post_id=-1):
     if not request.user.is_authenticated():
-        return HttpResponseRedirect('/')
+        paraDic={'errCode': 2}
+        return render(request,'test/error.html', paraDic)
 
     if post_id==-1:
         Post = Context(postName=request.POST['title'],contents=request.POST['contents'], userID=request.user,
@@ -78,18 +80,19 @@ def register(request):
     return render(request,'test/register.html',)
 
 def reguser(request):
-    # try:
     print(request.POST['regid'])
-    newUser=User.objects.create_user(request.POST['regid'],request.POST['email'],request.POST['regpwd'])
-    newUser.first_name=request.POST['firstName']
-    newUser.last_name=request.POST['lastName']
-    newUser.profile.field=request.POST['field']
-    newUser.profile.laguage=request.POST['language']
-    newUser.save()
-    # except 'UNIQUE constraint failed':
-    #     return HttpResponseRedirect(reverse('write'))
-    # else:
-    return HttpResponseRedirect(reverse('index'))
+    paraDic = {'username': request.POST['firstName'] + request.POST['lastName'], 'errCode': 3}
+    try:
+        newUser = User.objects.create_user(request.POST['regid'], request.POST['email'], request.POST['regpwd'])
+        newUser.first_name = request.POST['firstName']
+        newUser.last_name = request.POST['lastName']
+        # newUser.profile.field=request.POST['field']
+        # newUser.profile.laguage=request.POST['language']
+
+        newUser.save()
+        return render(request,'test/welcome.html', paraDic)
+    except:
+        return render(request,'test/error.html', paraDic)
 
 def viewPost(request,post_id):
     obj=Context.objects.get(postID=post_id)
@@ -128,7 +131,8 @@ def error(request):
 
 def detail(request,post_id):
     if not request.user.is_authenticated:
-        return HttpResponseRedirect('/')
+        paraDic={'errCode': 2}
+        return render(request,'test/error.html', paraDic)
 
     obj=Context.objects.get(postID=post_id)
 
@@ -154,7 +158,8 @@ def join(request, post_id):
         obj.member2=request.user.username
         obj.save()
     else:
-        pass
+        paraDic={'errCode': 1}
+        return render(request,'test/error.html', paraDic)
     #TODO: 슬롯이 다 찬 경우 예외처리 (에러페이지로 리다이렉트)
 
     return HttpResponseRedirect('/detail/' + str(post_id))
